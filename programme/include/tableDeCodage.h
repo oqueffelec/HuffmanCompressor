0dc3b1c7539a7d49988dbc6328a62459cde0e70e
octave <octave.queffelec@insa-rouen.fr>
2016-11-20 01:57:44 +0100

    tad tdc

---
+++ programme/include/tableDeCodage.h
@@ -0,0 +1,82 @@
+/**
+ * \file tableDeCodage.h
+ * \brief Implantation du TAD tableDeCodage : tableDeCodage est un dictionnaire qui a pour clé un octet et pour valeur un code binaire
+ * \author Octave Queffelec
+ * \version 1.0
+ */
+#ifndef __TABLE__DE__CODAGE__
+#define __TABLE__DE__CODAGE__
+
+#include "codeBinaire.h"
+#include "octet.h"
+
+
+/**
+* \struct TDC_tableDeCodage tableDeCodage.h
+* \brief la structure est une liste chainee ayant 2 elements (la clé et la valeur)
+*/
+
+typedef struct TDC_Noeud* TDC_tableDeCodage;
+typedef struct TDC_Noeud {
+	octet octet;
+  codeBinaire codeBinaire;
+	TDC_tableDeCodage listeSuivante;
+}  TDC_tableDeCodage;
+
+/**
+* \fn TDC_tableDeCodage TDC_tableDeCodage()
+* \brief renvoie la position d'un coup
+* \return TDC_tableDeCodage
+*/
+
+TDC_tableDeCodage TDC_tableDeCodage();
+
+/**
+* \fn void ajouter(TDC_tableDeCodage, octet, codeBinaire)
+* \brief ajoute un couple cle valeur à la TDC
+* \return void
+*/
+
+void TDC_ajouter(TDC_tableDeCodage* tdc, octet o, codeBinaire cb);
+
+/**
+* \fn TDC_estPresentOctet(TDC_tableDeCodage, octet)
+* \brief verifie la presence d'un octet dans la TDC
+* \return int
+*/
+
+int TDC_estPresentOctet(TDC_tableDeCodage tdc, octet o);
+
+/**
+* \fn int TDC_estPresentCodeBinaire(TDC_tableDeCodage tdc, codeBinaire cb)
+* \brief verifie la presence d'un codebinaire dans la TDC
+* \return int
+*/
+
+int TDC_estPresentCodeBinaire(TDC_tableDeCodage tdc, codeBinaire cb);
+
+/**
+* \fn octet TDC_obtenirOctet(TDC_tableDeCodage tdc, codeBinaire cb)
+* \brief renvoie l'octet correspondant
+* \return octet
+*/
+
+octet TDC_obtenirOctet(TDC_tableDeCodage tdc, codeBinaire cb);
+
+/**
+* \fn codeBinaire TDC_obtenirCodeBinaire(TDC_tableDeCodage tdc, octet o)
+* \brief renvoie le codebinaire correspondant
+* \return codebinaire
+*/
+
+codeBinaire TDC_obtenirCodeBinaire(TDC_tableDeCodage tdc, octet o);
+
+/**
+* \fn void TDC_retirerOctet(TDC_tableDeCodage tdc, octet o)
+* \brief retire le couple octet codebinaire
+* \return void
+*/
+
+void TDC_retirerOctet(TDC_tableDeCodage* tdc, octet o);
+
+#endif
---
+++ programme/src/tableDeCodage.c
@@ -0,0 +1,94 @@
+/**
+  * \file tableDeCodage.c
+  * \brief Conception du TAD tableDeCodage
+  * \author Octave Queffelec
+  * \version 1.0
+  * \date 20/11/2016
+  */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "tableDeCodage.h"
+
+#define TRUE 1
+#define FALSE 0
+
+TDC_tableDeCodage TDC_tableDeCodage(){
+  errno=0;
+  return NULL;
+}
+
+void TDC_ajouter(TDC_tableDeCodage* tdc, octet o, codeBinaire cb){
+  TDC_tableDeCodage pNoeud=(TDC_tableDeCodage)malloc(sizeof(TDC_Noeud));
+  if (pNoeud!=NULL) {
+      errno=0;
+      pNoeud->octet=o;
+      pNoeud->codeBinaire=cb;
+      pNoeud->listeSuivante=*tdc;
+      *tdc=pNoeud;
+  } else {
+    errno=TDC_ERREUR_MEMOIRE;
+  }
+}
+
+int TDC_estPresentOctet(TDC_tableDeCodage tdc, octet o){
+  if(tdc.listeSuivante!=NULL){
+    if(tdc.octet==o){
+      return TRUE;
+    }
+    else{
+      TDC_estPresentOctet(tdc.listeSuivante,o);
+    }
+  }
+  else
+    return FALSE;
+}
+
+int TDC_estPresentCodeBinaire(TDC_tableDeCodage tdc, codeBinaire cb){
+  if(tdc.listeSuivante!=NULL){
+    if(tdc.codeBinaire==cb){
+      return TRUE;
+    }
+    else{
+      TDC_estPresentOctet(tdc.listeSuivante,cb);
+    }
+  }
+  else
+    return FALSE;
+}
+
+
+
+octet TDC_obtenirOctet(TDC_tableDeCodage tdc, codeBinaire cb){
+  assert(TDC_estPresentCodeBinaire(tdc,cb));
+  if(tdc.listeSuivante!=NULL){
+    if(tdc.codeBinaire==cd)
+      return tdc.octet;
+    else
+      TDC_obtenirOctet(tdc.listeSuivante,cb);
+  }
+}
+
+codeBinaire TDC_obtenirCodeBinaire(TDC_tableDeCodage tdc, octet o){
+  assert(TDC_estPresentOctet(tdc,o));
+  if(tdc.listeSuivante!=NULL){
+    if(tdc.octet==o)
+      return tdc.codeBinaire;
+    else
+      TDC_obtenirCodeBinaire(tdc.listeSuivante,cb);
+  }
+}
+
+void TDC_retirerOctet(TDC_tableDeCodage* tdc, octet o){
+  assert(TDC_estPresentOctet(tdc,o));
+  codeBinaire cb=TDC_obtenirCodeBinaire(tdc,o);
+  TDC_tableDeCodage temp;
+  assert(tdc!=NULL);
+  errno=0;
+  temp=*tdc;
+  *tdc=tdc->listeSuivante;
+  free(temp);
+
+}
