b5ae08946425953495e36aed04a8fc2405d7341c
octave <octave.queffelec@insa-rouen.fr>
2016-11-21 22:05:55 +0100

    ajout tad bit et mode

---
+++ programme/include/bit.h
@@ -0,0 +1,16 @@
+/**
+ * \file bit.h
+ * \brief Implantation du TAD bit : type énumeré
+ * \author Octave Queffelec
+ * \version 1.0
+ */
+#ifndef __BIT__
+#define __BIT__
+
+typedef enum bit
+        {
+          bitA0,
+          bitA1
+        };
+
+#endif
---
+++ programme/include/mode.h
@@ -0,0 +1,16 @@
+/**
+ * \file mode.h
+ * \brief Implantation du TAD mode : type énumeré
+ * \author Octave Queffelec
+ * \version 1.0
+ */
+#ifndef __MODE__
+#define __MODE__
+
+typedef enum mode
+        {
+          lecture,
+          ecriture
+        };
+
+#endif
