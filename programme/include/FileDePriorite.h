932a939775d59a4e6a0c825d6bde353a639bc968
Regis Maskemde <regis.maskemde@insa-rouen.fr>
2016-12-14 08:37:32 +0100

    fileDePriorite

--- programme/include/FileDePriorite.h
+++ programme/include/FileDePriorite.h
@@ -3,26 +3,71 @@
 
 #define FDP_ERREUR_MEMOIRE 1
 
+#include <errno.h>
+#include "arbreDeHuffman.h"
+
 /**
-* \struct TDC_tableDeCodage tableDeCodage.h
-* \brief la structure est une liste chainee ayant 2 elements (la clé et la valeur)
+* \struct FDP_filedepriorite FileDePriorite.h
+* \brief la structure est un tableau dynamique d'arbres
+* L'enregistrement contient aussi la taille du tableau et le nombre d'elements
 */
+typedef struct fileDePriorite{
+	int taille;
+	int nbElements;
+    ArbreDeHuffman* Elements;
+} fileDePriorite;
 
-typedef struct FDP_Noeud* FDP_FileDEPriorite;
-typedef struct FDP_Noeud {
-	int* octet;
- 
-	FDP_FileDEPriorite listeSuivante;
-}  FDP_Noeud;
-
-
-FDP_FileDEPriorite FDP_fileDepriorite();
-
-
+/**
+* \fonction FDP_estVide
+* \arguments fdp une file de priorite 
+* \brief la fonction retoune 1 si la file de pririte est vide et 0 si elle n'est pas vide
+*/
 int FDP_estVide(fileDePriorite* fdp);
 
-int FDP_defiler(fileDePriorite* fdp, int*listeSuivante)
+/**
+* \fonction FDP_defiler 
+* \arguments fdp une file de priorite 
+* \brief la fonction retourne le plus grand element de la file et le suprimme de la file
+*/
+int FDP_defiler(fileDePriorite* fdp);
 
-int FDP_obtenirElement(fileDePriorite* fdp, int*o)
-int FDP_insérer(fileDePriorite* fdp, int*, int*o, int*cb)
+/**
+* \fonction FDP_insererElement 
+* \arguments fdp une file de priorite 
+             element l'arbre a inserer
+* \brief la fonction insere un arbre dans la file
+*/
+void FDP_insererElement(fileDePriorite* fdp, ArbreDeHuffman element);
+
+/**
+* \fonction FDP_initialiser
+* \arguments fdp une file de priorite
+* \brief la fonction initialise un file de priorite
+*/
+void FDP_intialiser(fileDePriorite* fdp);
+
+/**
+* \fonction ranger
+* \arguments fdp un tableau d'arbres de huffman 
+             tete l'indice du premier element
+	     nbElement le nombre d'elements a ranger
+* \brief la fonction trie recursivement le tableau d'arbres de façcon decroissante selon leur priorite (ponderation)
+*/
+void ranger(ArbreDeHuffman * fdp, int tete, int nbElements);
+
+/**
+* \fonction FDP_insererElement 
+* \arguments fdp une file de priorite 
+* \brief la fonction retourne le plus grand element de la liste
+*/
+ArbreDeHuffman FDP_ObtenirElement(fileDePriorite *fdp);
+
+/**
+* \fonction FDP_toutDefiler
+* \arguments fdp une file de priorite 
+             element l'arbre a inserer
+* \brief la fonction vide la file de priorite et desalloue tous les elements
+*/
+void FDP_toutDefiler(fileDePriorite *fdp);
 #endif
+
