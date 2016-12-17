b9d5993ce63a6a1b9014d016e27130b578aa2021
mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>
2016-12-17 13:35:22 +0100

    ajout fonction metier caractereEnCodeBinaire

---
+++ programme/include/caractereEnCodeBinaire.h
@@ -0,0 +1,20 @@
+/**
+ * \file caractereEnCodeBinaire.h
+ * \brief Implantation de la fonction caractereEnCodeBinaire : conversion char vers CB_CodeBinaire
+ * \author Mathieu Vandecasteele
+ * \version 1.0
+ * \date 16/12/2016
+ */
+#ifndef __CARACTEREENCB__
+#define __CARACTEREENCB__
+
+
+/**
+* \fn CB_CodeBinaire caractereEnCodeBinaire(char c)
+* \brief renvoie un CodeBinaire à partir d'un caractère
+* \return CB_CodeBinaire
+*/
+
+CB_CodeBinaire caractereEnCodeBinaire(char c);
+
+#endif
---
+++ programme/src/caractereEnCodeBinaire.c
@@ -0,0 +1,32 @@
+/**
+ * \file caractereEnCodeBinaire.c
+ * \brief Implantation de la fonction caractereEnCodeBinaire : conversion char vers CB_CodeBinaire
+ * \author Mathieu Vandecasteele
+ * \version 1.0
+ * \date 16/12/2016
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "bit.h"
+#include "codeBinaire.h"
+
+CB_CodeBinaire caractereEnCodeBinaire(char c){
+
+CB_CodeBinaire code;
+code = CB_codeBinaire();
+int nombre = c; //conversion caractère vers décimal
+
+for (int i = 8;i>-1;i--) { //conversion en CodeBinaire
+
+if (pow(2,i) > nombre){
+      CB_ajouter(&code,bitA1);
+        nombre = nombre-pow(2,i);
+}
+else{
+      CB_ajouter(&code,bitA0) ;
+}
+}
+
+return code;
+}
