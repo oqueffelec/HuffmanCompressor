8e3fd7a2dd99a577aa2ee5a5c5755eee9ca29314
jwacyk <jeangabriel.wacyk@insa-rouen.fr>
2016-12-29 16:38:25 +0100

    ajout crerTableDeCodage

---
+++ programme/include/creerTableDeCodage.h
@@ -0,0 +1,48 @@
+/**
+ * \file creerTableDeCodage.h
+ * \brief Implantation de la fonction creerTableDeCodage : crée la table de codage correspondante au statistiques donnés.
+ * \author Jean-Gabriel Wacyk
+ * \version 1.0
+ * \date 26/12/2016
+ */
+
+#ifndef __CREERTABLEDECODAGE__
+#define __CREERTABLEDECODAGE__
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "octet.h"
+#include "statistiques.h"
+#include "creerTableDeCodage.h"
+#include "codeBinaire.h"
+#include "FileDePriorite.h"
+#include "creerFileDePriorite.h"
+
+/**
+* \fn ArbreDeHuffman creerAbreDeHuffman(FDP_FileDePriorite file)
+* \brief renvoie l'arbre de Huffman créer à partir d'une file de priorite
+* \return ArbreDeHuffman
+*/
+
+ArbreDeHuffman creerAbreDeHuffman(FDP_FileDePriorite file);
+
+/**
+* \fn void remplirTableDeCodage(TDC_TableDeCodage tdc, ArbreDeHuffman arbre, CB_CodeBinaire code);
+* \brief remplie la table de codage à partir d'un arbre de Huffman
+* \return void
+*/
+
+
+void remplirTableDeCodage(TDC_TableDeCodage* tdc, ArbreDeHuffman arbre, CB_CodeBinaire* code);
+
+/**
+* \fn TDC_TableDeCodage creerTableDeCodage(STAT_Statistiques stats);
+* \brief remplie la table de codage à partir des stats
+* \return TDC_TableDeCodage
+*/
+
+TDC_TableDeCodage creerTableDeCodage(STAT_Statistiques stats);
+
+#endif
---
+++ programme/src/creerTableDeCodage.c
@@ -0,0 +1,55 @@
+/**
+ * \file creerTableDeCodage.c
+ * \brief Implantation de la fonction creerTableDeCodage : crée la table de codage correspondante au statistiques donnés.
+ * \author Jean-Gabriel Wacyk
+ * \version 1.0
+ * \date 26/12/2016
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "octet.h"
+#include "statistiques.h"
+#include "creerTableDeCodage.h"
+#include "codeBinaire.h"
+#include "FileDePriorite.h"
+#include "creerFileDePriorite.h"
+
+ArbreDeHuffman creerAbreDeHuffman(FDP_FileDePriorite file){
+  ArbreDeHuffman temp;
+  ArbreDeHuffman arbre1;
+  ArbreDeHuffman ardbre2;
+  while (!(FDP_longueur(file)==1)) {
+    arbre1 = FDP_obtenirADH(file);
+    FDP_defilerADH(&file);
+    arbre2 = FDP_obtenirADH(file);
+    FDP_defilerADH(&file);
+    temp = ADH_ajouterRacine(arbre1,arbre2);
+    FDP_enfilerADH(&file,temp);
+  }
+  return temp;
+}
+
+void remplirTableDeCodage(TDC_TableDeCodage* tdc, ArbreDeHuffman arbre, CB_CodeBinaire* code){
+  if (ADH_estUneFeuille(arbre)==1){
+    TDC_ajouter(tdc,ADH_obtenirCaractere(arbre),*code);
+    CB_supprimerTete(code);
+  }
+  else{
+    CB_ajouter(code,bitA0);
+    remplirTableDeCodage(tdc,ADH_obtenirFilsGauche(arbre),code);
+    CB_ajouter(code,bitA1);
+    remplirTableDeCodage(tdc,ADH_obtenirFilsDroit(arbre),code);
+    CB_supprimerTete(code);
+  }
+}
+
+TDC_TableDeCodage creerTableDeCodage(STAT_Statistiques stats){
+  CB_CodeBinaire code = CB_codeBinaire();
+  TDC_TableDeCodage res = TDC_tableDeCodage();
+  FDP_FileDePriorite file = creerFileDePriorite(stats);
+  ArbreDeHuffman arbre = creerAbreDeHuffman(file);
+  remplirTableDeCodage(&res,arbre,&code);
+  return res;
+}
